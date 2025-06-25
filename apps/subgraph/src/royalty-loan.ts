1b086e9f6f3bf60a9242460ed952499d08002fec
Bartek-str <bartlomiejstruzyk@gmail.com>
2025-06-25 13:31:36 +0200

    added handlers for factory and added handler for initialized loan contract

--- apps/subgraph/src/royalty-loan.ts
+++ apps/subgraph/src/royalty-loan.ts
@@ -1,8 +1,8 @@
-import { Initialized as InitializedEvent } from '../generated/RoyaltyLoan/RoyaltyLoanc';
-import { Initialized } from '../generated/schema';
+import { InitializedLoan } from '../generated/schema';
+import { Initialized as InitializedEvent } from '../generated/templates/RoyaltyLoan/RoyaltyLoan';
 
 export function handleInitialized(event: InitializedEvent): void {
-  const entity = new Initialized(
+  const entity = new InitializedLoan(
     event.transaction.hash.concatI32(event.logIndex.toI32()),
   );
   entity.version = event.params.version;
