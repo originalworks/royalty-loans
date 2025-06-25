cd3a25b1d1ebb99441d62eda572a1c0abc071857
Daniel Cybulka <danielcybulka@gmail.com>
2025-06-25 12:26:52 +0200

    auth0 backend

---
+++ apps/backend/src/auth/auth.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt.strategy';
+
+@Module({
+  imports: [PassportModule, JwtModule.register({})],
+  providers: [JwtStrategy],
+  exports: [],
+})
+export class AuthModule {}
