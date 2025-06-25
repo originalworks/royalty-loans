d6e81e0f84b00a2cb960ac5781d60de01f5e80a0
Bartek-str <bartlomiejstruzyk@gmail.com>
2025-06-25 15:44:57 +0200

    added loan provided handler

--- apps/subgraph/src/royalty-loan.ts
+++ apps/subgraph/src/royalty-loan.ts
@@ -1,15 +1,25 @@
-import { InitializedLoan } from '../generated/schema';
-import { Initialized as InitializedEvent } from '../generated/templates/RoyaltyLoan/RoyaltyLoan';
+import { LoanProvided, InitializedLoan } from '../generated/schema';
+import {
+  Initialized as InitializedEvent,
+  LoanProvided as LoanProvidedEvent,
+} from '../generated/templates/RoyaltyLoan/RoyaltyLoan';
 
 export function handleInitialized(event: InitializedEvent): void {
   const entity = new InitializedLoan(
     event.transaction.hash.concatI32(event.logIndex.toI32()),
   );
   entity.version = event.params.version;
-
   entity.blockNumber = event.block.number;
   entity.blockTimestamp = event.block.timestamp;
   entity.transactionHash = event.transaction.hash;
+  entity.save();
+}
 
+export function handleLoanProvided(event: LoanProvidedEvent): void {
+  const entity = new LoanProvided(event.address);
+  entity.lender = event.params.lender;
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
   entity.save();
 }
