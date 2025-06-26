001e6a4bfd55141d23129649d9c8fa5bd45f1a0c
Bartek Strużyk <bartlomiejstruzyk@gmail.com>
2025-06-26 14:15:30 +0200

    updated subgraph tests

--- apps/subgraph/tests/royalty-loan-factory.test.ts
+++ apps/subgraph/tests/royalty-loan-factory.test.ts
@@ -1,11 +1,10 @@
 import {
+  test,
   assert,
   describe,
-  test,
+  afterEach,
   clearStore,
-  beforeAll,
-  afterAll,
-} from 'matchstick-as/assembly/index';
+} from 'matchstick-as';
 import { BigInt } from '@graphprotocol/graph-ts';
 import { handleInitialized } from '../src/royalty-loan-factory';
 import { createInitializedEvent } from './royalty-loan-factory-utils';
@@ -14,31 +13,25 @@ import { createInitializedEvent } from './royalty-loan-factory-utils';
 // https://thegraph.com/docs/en/subgraphs/developing/creating/unit-testing-framework/#tests-structure
 
 describe('Describe entity assertions', () => {
-  beforeAll(() => {
-    const version = BigInt.fromI32(234);
-    const newInitializedEvent = createInitializedEvent(version);
-    handleInitialized(newInitializedEvent);
-  });
-
-  afterAll(() => {
-    clearStore();
-  });
+  afterEach(() => {
+    clearStore()
+  })
 
   // For more test scenarios, see:
   // https://thegraph.com/docs/en/subgraphs/developing/creating/unit-testing-framework/#write-a-unit-test
 
-  test('Initialized created and stored', () => {
-    assert.entityCount('Initialized', 1);
+  test('InitializedFactory created and stored', () => {
+    const version = BigInt.fromI32(1);
+    const newInitializedEvent = createInitializedEvent(version);
+    handleInitialized(newInitializedEvent);
 
     // 0xa16081f360e3847006db660bae1c6d1b2e17ec2a is the default address used in newMockEvent() function
+    assert.entityCount('InitializedFactory', 1)
     assert.fieldEquals(
-      'Initialized',
-      '0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1',
+      'InitializedFactory',
+      '0xa16081f360e3847006db660bae1c6d1b2e17ec2a01000000',
       'version',
-      '234',
+      '1',
     );
-
-    // More assert options:
-    // https://thegraph.com/docs/en/subgraphs/developing/creating/unit-testing-framework/#asserts
   });
 });
--- apps/subgraph/tests/royalty-loan.test.ts
+++ apps/subgraph/tests/royalty-loan.test.ts
@@ -1,11 +1,10 @@
 import {
+  test,
   assert,
   describe,
-  test,
+  afterEach,
   clearStore,
-  beforeAll,
-  afterAll,
-} from 'matchstick-as/assembly/index';
+} from 'matchstick-as';
 import { BigInt } from '@graphprotocol/graph-ts';
 import { handleInitialized } from '../src/royalty-loan';
 import { createInitializedEvent } from './royalty-loan-utils';
@@ -14,26 +13,23 @@ import { createInitializedEvent } from './royalty-loan-utils';
 // https://thegraph.com/docs/en/subgraphs/developing/creating/unit-testing-framework/#tests-structure
 
 describe('Describe entity assertions', () => {
-  beforeAll(() => {
-    const version = BigInt.fromI32(234);
-    const newInitializedEvent = createInitializedEvent(version);
-    handleInitialized(newInitializedEvent);
-  });
-
-  afterAll(() => {
-    clearStore();
-  });
+  afterEach(() => {
+    clearStore()
+  })
 
   // For more test scenarios, see:
   // https://thegraph.com/docs/en/subgraphs/developing/creating/unit-testing-framework/#write-a-unit-test
 
-  test('Initialized created and stored', () => {
-    assert.entityCount('Initialized', 1);
+  test('InitializedLoan created and stored', () => {
+    const version = BigInt.fromI32(234);
+    const newInitializedEvent = createInitializedEvent(version);
+    handleInitialized(newInitializedEvent);
 
     // 0xa16081f360e3847006db660bae1c6d1b2e17ec2a is the default address used in newMockEvent() function
+    assert.entityCount('InitializedLoan', 1);
     assert.fieldEquals(
-      'Initialized',
-      '0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1',
+      'InitializedLoan',
+      '0xa16081f360e3847006db660bae1c6d1b2e17ec2a01000000',
       'version',
       '234',
     );
