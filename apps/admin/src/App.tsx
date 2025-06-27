b3843d1218761b1dd55fb7325441f3b6faf85900
Bartek-str <bartlomiejstruzyk@gmail.com>
2025-06-27 11:34:37 +0200

    working on web3 integration

--- apps/admin/src/App.tsx
+++ apps/admin/src/App.tsx
@@ -1,3 +1,5 @@
+import Web3 from "web3";
+import Web3Modal from "web3modal";
 import {
   Refine,
   GitHubBanner,
@@ -43,6 +45,16 @@ import { ColorModeContextProvider } from './contexts/color-mode';
 import { Header } from './components/header';
 import { Login } from './pages/login';
 
+export const TOKEN_KEY = "refine-auth";
+
+const providerOptions = {};
+const web3Modal = new Web3Modal({
+  cacheProvider: true,
+  providerOptions,
+});
+
+let provider: any | null = null;
+
 function App() {
   const { isLoading, user, logout, getIdTokenClaims } = useAuth0();
 
@@ -55,11 +67,31 @@ function App() {
 
   const authProvider: AuthProvider = {
     login: async () => {
-      return {
-        success: true,
-      };
+      if (window.ethereum) {
+        provider = await web3Modal.connect();
+        const web3 = new Web3(provider);
+        const accounts = await web3.eth.getAccounts();
+        localStorage.setItem(TOKEN_KEY, accounts[0]);
+        return {
+          success: true,
+        };
+      } else {
+        return {
+          success: false,
+          error: new Error(
+            'Not set ethereum wallet or invalid. You need to install Metamask',
+          ),
+        };
+      }
     },
     logout: async () => {
+      localStorage.removeItem(TOKEN_KEY);
+      if (provider && provider.close) {
+        await provider.close;
+
+        provider = null;
+        await web3Modal.clearCachedProvider();
+      }
       logout({ returnTo: window.location.origin });
       return {
         success: true,
@@ -70,9 +102,10 @@ function App() {
       return { error };
     },
     check: async () => {
+      const web3Token = localStorage.getItem(TOKEN_KEY);
       try {
         const token = await getIdTokenClaims();
-        if (token) {
+        if (token && web3Token) {
           axios.defaults.headers.common = {
             Authorization: `Bearer ${token.__raw}`,
           };
