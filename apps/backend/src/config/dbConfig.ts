0e9756d7deaf90d279de3a15969da845899ab96d
Daniel Cybulka <danielcybulka@gmail.com>
2025-07-01 17:50:51 +0200

    add infra & migrations

--- apps/backend/src/config/dbConfig.ts
+++ apps/backend/src/config/dbConfig.ts
@@ -1,15 +1,57 @@
 import { DataSourceOptions } from 'typeorm';
 import { entities } from './entities';
 
-export const testDbConfig = (): DataSourceOptions => ({
-  host: process.env.DB_HOST,
-  port: 5432,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  type: 'postgres',
-  entities,
-  migrations: ['dist/migrations/*.js'],
-  synchronize: true,
-  dropSchema: true,
-});
+export type DbConfigs = 'db' | 'test' | 'local' | 'migration';
+
+export const dbConfigs: { [k in DbConfigs]: DataSourceOptions } = {
+  db: {
+    host: process.env.DB_HOST,
+    type: 'postgres',
+    port: parseInt(process.env.DB_PORT || '5432'),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    entities,
+    migrations: ['out/dist/migrations/*.js'],
+    synchronize: false,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
+  local: {
+    host: process.env.DB_HOST,
+    port: 5432,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    type: 'postgres',
+    entities,
+    migrations: ['infrastructure/out/dist/migrations/*.js'],
+    synchronize: false,
+  },
+  test: {
+    host: process.env.DB_HOST,
+    port: 5432,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    type: 'postgres',
+    entities,
+    migrations: ['dist/migrations/*.js'],
+    synchronize: true,
+    dropSchema: true,
+  },
+  migration: {
+    name: 'db-creator',
+    host: process.env.DB_HOST,
+    type: 'postgres',
+    port: parseInt(process.env.DB_PORT || '5432'),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.ROOT_DB_NAME,
+    synchronize: false,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
+};
--- apps/backend/src/migrations/1751379607529-InitialMigration.ts
+++ apps/backend/src/migrations/1751379607529-InitialMigration.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class InitialMigration1751287684252 implements MigrationInterface {
-  name = 'InitialMigration1751287684252';
+export class InitialMigration1751379607529 implements MigrationInterface {
+  name = 'InitialMigration1751379607529';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
@@ -10,7 +10,7 @@ export class InitialMigration1751287684252 implements MigrationInterface {
                 "collateralTokenAddress" character varying NOT NULL,
                 "feePercentagePpm" bigint NOT NULL,
                 "maxLoanAmount" bigint NOT NULL,
-                "ratio" integer NOT NULL,
+                "ratio" numeric(5, 2) NOT NULL,
                 "createdAt" TIMESTAMP NOT NULL DEFAULT now(),
                 "updatedAt" TIMESTAMP NOT NULL DEFAULT now(),
                 CONSTRAINT "PK_de5d46c7031dc3dd3eec9821f13" PRIMARY KEY ("id")
