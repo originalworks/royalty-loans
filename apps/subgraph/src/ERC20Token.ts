3d0c802ce62317eebe976153d106435823cefa8a
Daniel Cybulka <danielcybulka@gmail.com>
2025-07-22 16:28:07 +0200

    add expenses to subgraph

---
+++ apps/subgraph/src/ERC20Token.ts
@@ -0,0 +1,22 @@
+import {
+  Approval as ApprovalEvent,
+  Transfer as TransferEvent,
+} from '../generated/ERC20Token/ERC20';
+import { LoanContract } from '../generated/schema';
+import { createExpense } from './expense';
+
+export function handleERC20Transfer(event: TransferEvent): void {
+  const loanContract = LoanContract.load(event.params.to);
+
+  if (loanContract !== null) {
+    createExpense(event.transaction.hash, loanContract.id, 'ERC20Transfer');
+  }
+}
+
+export function handleERC20Approval(event: ApprovalEvent): void {
+  const loanContract = LoanContract.load(event.params.spender);
+
+  if (loanContract !== null) {
+    createExpense(event.transaction.hash, loanContract.id, 'ERC20Approve');
+  }
+}
