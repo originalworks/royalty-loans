5a4fca1e0a47db3db9839438f83350d817cd5c54
Bartek-str <bartlomiejstruzyk@gmail.com>
2025-07-24 10:33:00 +0200

    added customizated pagination to list loan offers

--- apps/admin/src/pages/loan-offers/queries.ts
+++ apps/admin/src/pages/loan-offers/queries.ts
@@ -1,8 +1,23 @@
 import gql from 'graphql-tag';
 
+export const STATISTICS_QUERY = gql`
+  query Statistic($id: ID!) {
+    stats(id: $id) {
+      id
+      contractsCount
+      expensesCount
+    }
+  }
+`;
+
 export const LOAN_OFFERS_LIST_QUERY = gql`
   query LoanOffersList($first: Int, $skip: Int) {
-    loanContracts(first: $first, skip: $skip, orderBy: timestamp, orderDirection: desc) {
+    loanContracts(
+      first: $first
+      skip: $skip
+      orderBy: timestamp
+      orderDirection: desc
+    ) {
       id
       loanContract
       borrower
