8b00bd1ec9274812822ce7ad598d6da59d1a94b5
Bartek-str <bartlomiejstruzyk@gmail.com>
2025-08-04 12:43:31 +0200

    added fields and columns to show network everywhere

--- apps/admin/src/pages/transactions/show.tsx
+++ apps/admin/src/pages/transactions/show.tsx
@@ -1,4 +1,5 @@
 import { ethers } from 'ethers';
+import { useChainId, useChains } from 'wagmi';
 
 import {
   Show,
@@ -13,6 +14,8 @@ import { TRANSACTION_SHOW_QUERY } from '../queries';
 
 export const TransactionShow = () => {
   const { id } = useParsed();
+  const chainId = useChainId();
+  const chains = useChains();
   const dataProvider = useDataProvider(true);
 
   const { query } = useShow({
@@ -28,6 +31,8 @@ export const TransactionShow = () => {
 
   const record = data?.data;
 
+  const foundChain = chains.find((chain) => chain.id === chainId);
+
   return (
     <Show isLoading={isLoading}>
       <Stack gap={1}>
@@ -35,6 +40,15 @@ export const TransactionShow = () => {
           ID
         </Typography>
         <TextField value={record?.id} />
+
+        {foundChain && (
+          <>
+            <Typography variant="body1" fontWeight="bold">
+              Network
+            </Typography>
+            <TextField value={foundChain.name} />
+          </>
+        )}
 
         <Typography variant="body1" fontWeight="bold">
           Contract Address
