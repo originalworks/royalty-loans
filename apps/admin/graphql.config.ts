94022cd34e4f6d481a835aca571b36c95c91e362
Bartek-str <bartlomiejstruzyk@gmail.com>
2025-08-12 12:30:11 +0200

    fixed issue with wrong status check

--- apps/admin/graphql.config.ts
+++ apps/admin/graphql.config.ts
@@ -1,7 +1,7 @@
 import type { IGraphQLConfig } from 'graphql-config';
 
 const config: IGraphQLConfig = {
-  schema: process.env.VITE_SUBGRAPH_URL,
+  schema: process.env.VITE_BASE_SUBGRAPH_URL,
   extensions: {
     codegen: {
       hooks: {
