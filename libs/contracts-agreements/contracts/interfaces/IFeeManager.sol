3245e4b525b34c0c56c4ab8ae88162c71ff59283
cezary-stroczynski <cezary.stroczynski@gmail.com>
2025-12-30 14:36:57 +0100

    Agreements - FeeManager.sol code update

--- libs/contracts-agreements/contracts/interfaces/IFeeManager.sol
+++ libs/contracts-agreements/contracts/interfaces/IFeeManager.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 interface IFeeManager {
-    function getFees() external view returns (uint256, uint256, uint256);
+  function getFees() external view returns (uint256, uint256, uint256);
 
-    function owner() external view returns (address);
+  function owner() external view returns (address);
 }
