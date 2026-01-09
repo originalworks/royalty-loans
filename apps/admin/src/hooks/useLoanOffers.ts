b66baaf6b22c8c47730b3fa0a57db0db40c8d682
Bartek Strużyk <bartlomiejstruzyk@gmail.com>
2026-01-09 14:06:07 +0100

    added batch transactions in provide loan function

--- apps/admin/src/hooks/useLoanOffers.ts
+++ apps/admin/src/hooks/useLoanOffers.ts
@@ -1,6 +1,9 @@
 import {
+  sendCalls,
   readContract,
   writeContract,
+  getCapabilities,
+  waitForCallsStatus,
   waitForTransactionReceipt,
 } from 'wagmi/actions';
 import { useState } from 'react';
@@ -76,41 +79,72 @@ export const useLoanOffers = () => {
           args: [],
         });
 
-        if (!paymentToken) return;
+        if (!paymentToken || !chainId) return;
 
-        const approveHash = await writeContract(config, {
-          chainId,
-          abi: erc20Abi,
-          address: paymentToken,
-          functionName: 'approve',
-          args: [loanContract, MaxUint256],
-        });
+        const capabilites = await getCapabilities(config);
+        if (
+          capabilites?.[chainId]?.atomic?.status === 'supported' ||
+          capabilites?.[chainId]?.atomic?.status === 'ready'
+        ) {
+          const id = await sendCalls(config, {
+            calls: [
+              {
+                abi: erc20Abi,
+                to: paymentToken,
+                functionName: 'approve',
+                args: [loanContract, MaxUint256],
+              },
+              {
+                abi: royaltyLoanAbi,
+                to: loanContract,
+                functionName: 'provideLoan',
+                args: [],
+              },
+            ],
+            forceAtomic: true, // if user rejects to use smart wallet and value is true then everything is reverted
+          });
 
-        const { status: approveStatus } = await waitForTransactionReceipt(
-          config,
-          {
-            hash: approveHash,
-          },
-        );
+          const { status } = await waitForCallsStatus(config, id);
 
-        if (approveStatus !== 'success') return;
+          if (status === 'success') {
+            setTimeout(() => navigate(0), 3000);
+          }
+          if (status === 'failure') console.error('Transaction reverted');
+        } else {
+          const approveHash = await writeContract(config, {
+            chainId,
+            abi: erc20Abi,
+            address: paymentToken,
+            functionName: 'approve',
+            args: [loanContract, MaxUint256],
+          });
 
-        const hash = await writeContract(config, {
-          chainId,
-          abi: royaltyLoanAbi,
-          address: loanContract,
-          functionName: 'provideLoan',
-          args: [],
-        });
+          const { status: approveStatus } = await waitForTransactionReceipt(
+            config,
+            {
+              hash: approveHash,
+            },
+          );
 
-        const { status } = await waitForTransactionReceipt(config, {
-          hash: hash,
-        });
+          if (approveStatus !== 'success') return;
 
-        if (status === 'success') {
-          setTimeout(() => navigate(0), 3000);
+          const hash = await writeContract(config, {
+            chainId,
+            abi: royaltyLoanAbi,
+            address: loanContract,
+            functionName: 'provideLoan',
+            args: [],
+          });
+
+          const { status } = await waitForTransactionReceipt(config, {
+            hash: hash,
+          });
+
+          if (status === 'success') {
+            setTimeout(() => navigate(0), 3000);
+          }
+          if (status === 'reverted') console.error('Transaction reverted');
         }
-        if (status === 'reverted') console.error('Transaction reverted');
       } catch (error) {
         console.error(error);
       }
