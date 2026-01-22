c44d482d4a9f9fb159067dbc04d297ca48b5cde2
Daniel Cybulka <danielcybulka@gmail.com>
2026-01-22 10:43:33 +0100

    fix hardhat & deployment scripts

--- libs/contracts-loans/scripts/deployment.ts
+++ libs/contracts-loans/scripts/deployment.ts
@@ -1,5 +1,4 @@
 import hre from 'hardhat';
-import { deployWhitelist } from './Whitelist';
 import { deployRoyaltyLoanFactory } from './RoyaltyLoanFactory';
 import { saveNewDeployment } from './deploymentUtils';
 
@@ -10,36 +9,19 @@ const main = async () => {
     throw new Error('USDC address not set');
   }
 
-  let whitelistAddress = hre.network.config.defaultWhitelist;
-
-  if (!whitelistAddress) {
-    console.log('Deploying whitelists...');
-    const whitelistDeployment = await deployWhitelist(deployer, [
-      deployer.address,
-      '0x90DA1d45b73d975CCFfFC7619cEd34443681e506',
-      '0x78F2588052e48Be9bB74114b355423161e026Eea',
-      '0x35afa24e1d9375934ac0749ebad4ca9b57cafb73',
-      '0xB964Af78353fA64acD76EAfBD63D8c4409522f79',
-    ]);
-    whitelistAddress = await whitelistDeployment.contract.getAddress();
-    console.log('Done');
-  } else {
-    console.log('Using whitelist from config...');
-  }
-
   console.log('Deploying loanFactory...');
   const factoryDeployment = await deployRoyaltyLoanFactory(
     deployer,
-    whitelistAddress,
     paymentTokenAddress,
   );
   console.log('Done!');
 
   const deploymentFile = {
     deployer: deployer.address,
-    royaltyLoanTemplate: factoryDeployment.royaltyLoanTemplate,
+    standardRoyaltyLoanTemplate: factoryDeployment.standardRoyaltyLoanTemplate,
+    beneficiaryRoyaltyLoanTemplate:
+      factoryDeployment.beneficiaryRoyaltyLoanTemplate,
     royaltyLoanFactory: factoryDeployment.royaltyLoanFactory,
-    whitelist: whitelistAddress,
     paymentToken: paymentTokenAddress,
   };
 
